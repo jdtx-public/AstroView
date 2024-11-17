b6163e18f6e5632b7722659927acf37345c1116d
Jeff Doar <jdoar@swbell.net>
2024-11-17 13:01:24 -0600

    intro commit of View2

---
+++ View2/AstroView2/AstroView2/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  AstroView2
+//
+//  Created by Jeff Doar on 11/16/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
---
+++ View2/AstroView2/AstroView2Tests/AstroView2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  AstroView2Tests.swift
+//  AstroView2Tests
+//
+//  Created by Jeff Doar on 11/16/24.
+//
+
+import Testing
+@testable import AstroView2
+
+struct AstroView2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
