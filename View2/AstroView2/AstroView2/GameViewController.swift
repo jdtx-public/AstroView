d13ee271c67818aee439fe6d9f6cfc56aef4c74f
Jeff Doar <jdoar@swbell.net>
2024-11-23 18:04:26 -0600

    add NovasC files and compute earth position

--- View2/AstroView2/AstroView2/GameViewController.swift
+++ View2/AstroView2/AstroView2/GameViewController.swift
@@ -10,6 +10,9 @@ import QuartzCore
 
 class GameViewController: NSViewController {
     
+    private static let earthRadius: Double = 6378137
+    private static let earthMass: Double = 5.97219e24
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,8 +30,8 @@ class GameViewController: NSViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 200)
-        cameraNode.camera?.zFar = 30000;
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 109 * 1.3 * GameViewController.earthRadius)
+        cameraNode.camera?.zFar = 109 * 3 * GameViewController.earthRadius
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -103,30 +106,46 @@ class GameViewController: NSViewController {
     }
     
     @IBAction func handleViewEarth(_ sender: Any) {
-        let scnView = self.view as! SCNView
-        let scene = scnView.scene!
-        
-        let cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true)!
-        
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 23464)
+        viewByName(bodyName: "Earth")
     }
     
     @IBAction func handleViewSun(_ sender: Any) {
+        viewByName(bodyName: "Sun")
+    }
+    
+    private func viewByName(bodyName: String) {
         let scnView = self.view as! SCNView
         let scene = scnView.scene!
         
-        let cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true)!
+        let bodyNode = scene.rootNode.childNode(withName: bodyName, recursively: true)!
+        let bodyZ = bodyNode.position.z
+        let bodyBounds = bodyNode.geometry!.boundingBox
         
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 200)
+        let cameraNode = scene.rootNode.childNode(withName: "camera", recursively: true)!
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: bodyZ + bodyBounds.max.z + 1000)
     }
     
     private class func addSolarBodies(targetNode: SCNNode) {
-        targetNode.addChildNode(solarSystemBody(bodyName: "Sun", earthMassFraction: 333030, earthRadiusFraction: 109, zInitial: 0, textureName: "Solarsystemscope_texture_8k_sun"));
-        targetNode.addChildNode(solarSystemBody(bodyName: "Earth", earthMassFraction: 1, earthRadiusFraction: 1, zInitial: 23454.8, textureName: "earth_texture"));
+        let earthRadiusOrbit = 23454.8
+        targetNode.addChildNode(solarSystemBody(bodyName: "Sun", earthMassFraction: 333030, earthRadiusFraction: 109, earthRadiusOrbit: 0, textureName: "Solarsystemscope_texture_8k_sun"));
+        
+        let earthNode = solarSystemBody(bodyName: "Earth", earthMassFraction: 1, earthRadiusFraction: 1, earthRadiusOrbit: earthRadiusOrbit, textureName: "earth_texture")
+        targetNode.addChildNode(earthNode);
+        
+        let earthPos = PlanetSim.earthPos(d: Date.now)
+
+        let fullDistance = earthRadiusOrbit * GameViewController.earthRadius
+
+        earthNode.position = SCNVector3(earthPos.x * fullDistance, earthPos.y * fullDistance, earthPos.z * fullDistance)
     }
     
-    private class func solarSystemBody(bodyName: String, earthMassFraction: Double, earthRadiusFraction: Double, zInitial: Double, textureName: String) -> SCNNode {
-        let sphere = SCNSphere(radius: earthRadiusFraction)
+    private class func solarSystemBody(bodyName: String, earthMassFraction: Double, earthRadiusFraction: Double,
+                                       earthRadiusOrbit: Double, textureName: String) -> SCNNode {
+        let fullRadius = earthRadiusFraction * GameViewController.earthRadius
+        let fullDistance = earthRadiusOrbit * GameViewController.earthRadius
+        let fullMass = earthMassFraction * GameViewController.earthMass
+        
+        let sphere = SCNSphere(radius: fullRadius)
         let node = SCNNode( geometry: sphere)
         var textureMaterial = SCNMaterial()
         let mainBundle = Bundle.main
@@ -135,7 +154,7 @@ class GameViewController: NSViewController {
         textureMaterial.diffuse.contents = myImage
         node.geometry?.materials = [textureMaterial]
         node.name = bodyName
-        node.position = SCNVector3(x: 0, y: 0, z: zInitial)
+        node.position = SCNVector3(x: 0, y: 0, z: fullDistance)
         return node
     }
 }
