43a76dcf4cb37eca4f1bf08a4ce71cc5d7dc3555
JD71 <48869533+JD71@users.noreply.github.com>
2024-12-19 21:17:36 -0600

    add a spice based system model - not working fully yet (#5)
    * started on the SpiceSystemModel
    
    - planets before Mars don't work - the orbits are all wacky (probably because of the need to convert units)
    - planets beyond Mars don't work; spice doesn't think it has enough data
    - I omitted the kernels for now because they're huge
    
    * convert spice geometry (km) to
    scene geometry (earth radii)

--- AstroView2/Physics/SpiceSystemModel.swift
+++ AstroView2/Physics/SpiceSystemModel.swift
@@ -1,5 +1,5 @@
 //
-//  NovasSystemModel.swift
+//  SpiceSystemModel.swift
 //  AstroView2
 //
 //  Created by Jeff Doar on 12/16/24.
@@ -7,19 +7,26 @@
 
 import Foundation
 import simd
-import novas_swift
+import cspice_swift
 
-public class NovasSystemModel: SystemModel {
-    private let _planetSim: PlanetSim
+public class SpiceSystemModel : SystemModel {
     private let _bodyRecords: [BodyRecord]
-    
+    private let _solarSystem: SolarSystem
+    private let _kernels: [Kernel]
+
     public init() {
         let mainBundle = Bundle.main
-        let resourcePath = mainBundle.path(forResource: "lnxp1900p2053", ofType: "421", inDirectory: "ephemeris")!
         
-        _planetSim = PlanetSim(ephemerisPath: resourcePath)
+        let bsps = [ "de432s", "de438", "de440" ]
         
-        _bodyRecords = NovasSystemModel.createBodyRecords()
+        let resourcePaths : [String] = bsps.map { mainBundle.path(forResource: $0, ofType: "bsp", inDirectory: "spice")! }
+
+        var localKernels : [Kernel] = resourcePaths.map { Kernel(withFilePath: $0) }
+        
+        _kernels = localKernels
+        _solarSystem = SolarSystem()
+        
+        _bodyRecords = SpiceSystemModel.createBodyRecords()
     }
     
     public func forEachBody(_ body: (BodyRecord) throws -> Void) throws {
@@ -27,28 +34,12 @@ public class NovasSystemModel: SystemModel {
     }
     
     public func sunRelativePosition(forBody body: BodyRecord, atTime time: Date) -> simd_double3 {
-        switch body.name {
-        case "Sun":
-            return _planetSim.sunPos(d: time)
-        case "Mercury":
-            return _planetSim.mercuryPos(d: time)
-        case "Venus":
-            return _planetSim.venusPos(d: time)
-        case "Earth":
-            return _planetSim.earthPos(d: time)
-        case "Mars":
-            return _planetSim.marsPos(d: time)
-        case "Jupiter":
-            return _planetSim.jupiterPos(d: time)
-        case "Saturn":
-            return _planetSim.saturnPos(d: time)
-        case "Uranus":
-            return _planetSim.uranusPos(d: time)
-        case "Neptune":
-            return _planetSim.neptunePos(d: time)
-        default:
-            return simd_double3.zero
-        }
+        let planetBody = _solarSystem[body.name]!
+
+        // spice gives answers in KM; we expect answers in earth radii
+        let posKm = planetBody.solarPosition(atDate: time)
+        
+        return posKm.kmToEarthRadii
     }
     
     private class func createBodyRecords() -> [BodyRecord] {
@@ -57,11 +48,13 @@ public class NovasSystemModel: SystemModel {
             BodyRecord(name: "Mercury", path: ":Sun:", earthRadiusFraction: 0.3829, texturePath: "Solarsystemscope_texture_8k_mercury"),
             BodyRecord(name: "Venus", path: ":Sun:", earthRadiusFraction: 0.3829, texturePath: "2k_venus_surface"),
             BodyRecord(name: "Earth", path: ":Sun:", earthRadiusFraction: 1, texturePath: "Solarsystemscope_texture_8k_earth_daymap"),
+            /*
             BodyRecord(name: "Mars", path: ":Sun:", earthRadiusFraction: 0.533, texturePath: "2k_mars"),
             BodyRecord(name: "Jupiter", path: ":Sun:", earthRadiusFraction: 11.21, texturePath: "2k_jupiter"),
             BodyRecord(name: "Saturn", path: ":Sun:", earthRadiusFraction: 9.45, texturePath: "2k_saturn"),
             BodyRecord(name: "Uranus", path: ":Sun:", earthRadiusFraction: 4.01, texturePath: "2k_uranus"),
             BodyRecord(name: "Neptune", path: ":Sun:", earthRadiusFraction: 3.88, texturePath: "2k_neptune"),
+            */
         ]
         
         return records
