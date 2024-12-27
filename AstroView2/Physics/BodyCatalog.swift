63bf48184f9c8ae1c01a9fd6c970c20233019e55
Jeff Doar <jdoar@swbell.net>
2024-12-26 21:11:36 -0600

    - add moon record - change path to just parent body name

--- AstroView2/Physics/BodyCatalog.swift
+++ AstroView2/Physics/BodyCatalog.swift
@@ -23,16 +23,14 @@ public class BodyCatalog {
             return nil
         }
         
-        let parentBodyName = body.name.split(separator: ":").last
+        let parentBodyName = body.path
         
-        if ((parentBodyName == nil) || (parentBodyName?.isEmpty == true)) {
+        if (parentBodyName.isEmpty == true) {
             return nil
         }
         
-        let parentBodyString = String(parentBodyName!)
-        
-        if _bodies.contains(where: { $0.value.name == parentBodyString }) {
-            return _bodies[parentBodyString]
+        if _bodies.contains(where: { $0.value.name == parentBodyName }) {
+            return _bodies[parentBodyName]
         }
         
         return nil
@@ -44,15 +42,16 @@ public class BodyCatalog {
 
     private class func createBodyRecords() -> [BodyRecord] {
         let records : [BodyRecord] = [
-            BodyRecord(name: "Sun", path: ":", earthRadiusFraction: 109, texturePath: "Solarsystemscope_texture_8k_sun", orbitalPeriodEarthYears: 0.0),
-            BodyRecord(name: "Mercury", path: ":Sun:", earthRadiusFraction: 0.3829, texturePath: "Solarsystemscope_texture_8k_mercury", orbitalPeriodEarthYears: 0.2408467),
-            BodyRecord(name: "Venus", path: ":Sun:", earthRadiusFraction: 0.3829, texturePath: "2k_venus_surface", orbitalPeriodEarthYears: 0.61519726),
-            BodyRecord(name: "Earth", path: ":Sun:", earthRadiusFraction: 1, texturePath: "Solarsystemscope_texture_8k_earth_daymap", orbitalPeriodEarthYears: 1.0000174),
-            BodyRecord(name: "Mars", path: ":Sun:", earthRadiusFraction: 0.533, texturePath: "2k_mars", orbitalPeriodEarthYears: 1.8808476),
-            BodyRecord(name: "Jupiter", path: ":Sun:", earthRadiusFraction: 11.21, texturePath: "2k_jupiter", orbitalPeriodEarthYears: 11.862615),
-            BodyRecord(name: "Saturn", path: ":Sun:", earthRadiusFraction: 9.45, texturePath: "2k_saturn", orbitalPeriodEarthYears: 29.447498),
-            BodyRecord(name: "Uranus", path: ":Sun:", earthRadiusFraction: 4.01, texturePath: "2k_uranus", orbitalPeriodEarthYears: 84.016846),
-            BodyRecord(name: "Neptune", path: ":Sun:", earthRadiusFraction: 3.88, texturePath: "2k_neptune", orbitalPeriodEarthYears: 164.79132),
+            BodyRecord(name: "Sun", path: "", earthRadiusFraction: 109, texturePath: "Solarsystemscope_texture_8k_sun", orbitalPeriodEarthYears: 0.0, bodyType: .Sun),
+            BodyRecord(name: "Mercury", path: "Sun", earthRadiusFraction: 0.3829, texturePath: "Solarsystemscope_texture_8k_mercury", orbitalPeriodEarthYears: 0.2408467),
+            BodyRecord(name: "Venus", path: "Sun", earthRadiusFraction: 0.3829, texturePath: "2k_venus_surface", orbitalPeriodEarthYears: 0.61519726),
+            BodyRecord(name: "Earth", path: "Sun", earthRadiusFraction: 1, texturePath: "Solarsystemscope_texture_8k_earth_daymap", orbitalPeriodEarthYears: 1.0000174),
+            BodyRecord(name: "Moon", path: "Earth", earthRadiusFraction: 0.2721, texturePath: "2k_moon", orbitalPeriodEarthYears: 1.0000174, bodyType: .NaturalSatellite),
+            BodyRecord(name: "Mars", path: "Sun", earthRadiusFraction: 0.533, texturePath: "2k_mars", orbitalPeriodEarthYears: 1.8808476),
+            BodyRecord(name: "Jupiter", path: "Sun", earthRadiusFraction: 11.21, texturePath: "2k_jupiter", orbitalPeriodEarthYears: 11.862615),
+            BodyRecord(name: "Saturn", path: "Sun", earthRadiusFraction: 9.45, texturePath: "2k_saturn", orbitalPeriodEarthYears: 29.447498),
+            BodyRecord(name: "Uranus", path: "Sun", earthRadiusFraction: 4.01, texturePath: "2k_uranus", orbitalPeriodEarthYears: 84.016846),
+            BodyRecord(name: "Neptune", path: "Sun", earthRadiusFraction: 3.88, texturePath: "2k_neptune", orbitalPeriodEarthYears: 164.79132),
         ]
         
         return records
