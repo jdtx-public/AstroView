c8df81190d80abecb8b9686665aee7730c19a6da
Jeff Doar <jdoar@swbell.net>
2025-09-13 19:50:31 -0500

    initial app

---
+++ AstroRk/AstroRk/AstroRkApp.swift
@@ -0,0 +1,19 @@
+//
+//  AstroRkApp.swift
+//  AstroRk
+//
+//  Created by Jeff Doar on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AstroRkApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
